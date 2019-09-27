fca6d61e9e80f949f60c645c946d8dcb51845bef
gkzz <gakuji.tamaki@gmail.com>
2019-09-27 19:19:47 +0900

    add params, "Extension fields"

--- images/stackstorm/bin/entrypoint.sh
+++ images/stackstorm/bin/entrypoint.sh
@@ -72,12 +72,12 @@ fi
 
 ( cd /etc/nginx/conf.d && ln -sf st2-base.cnf st2.conf )
 
-export ST2_API_KEY=$(st2 apikey create -k -m '{"name": "'$YOURBOTNAME'"}')
-sed -i -e 's/export ST2_API_KEY="${ST2_API_KEY}"/export ST2_API_KEY='"$ST2_API_KEY"'/' \
-    -i -e 's/# export HUBOT_ADAPTER=slack/export HUBOT_ADAPTER=slack/' \
-    -i -e 's/# export HUBOT_SLACK_TOKEN=xoxb-CHANGE-ME-PLEASE/export HUBOT_SLACK_TOKEN='"$HUBOT_SLACK_TOKEN"'/' \
-    /opt/stackstorm/chatops/st2chatops.env
-
-st2ctl reload --register-all && service st2chatops restart
+#export ST2_API_KEY=$(st2 apikey create -k -m '{"name": "'$YOURBOTNAME'"}')
+#sed -i -e 's/export ST2_API_KEY="${ST2_API_KEY}"/export ST2_API_KEY='"$ST2_API_KEY"'/' \
+#    -i -e 's/# export HUBOT_ADAPTER=slack/export HUBOT_ADAPTER=slack/' \
+#    -i -e 's/# export HUBOT_SLACK_TOKEN=xoxb-CHANGE-ME-PLEASE/export HUBOT_SLACK_TOKEN='"$HUBOT_SLACK_TOKEN"'/' \
+#    /opt/stackstorm/chatops/st2chatops.env
+#
+#st2ctl reload --register-all && service st2chatops restart
 
 exec /sbin/init
