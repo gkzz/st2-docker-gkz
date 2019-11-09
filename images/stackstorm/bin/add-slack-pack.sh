7ad1df0b9f9df671f509be907004f850cc0aab2c
gkzz <gakuji.tamaki@gmail.com>
2019-11-09 21:26:49 +0900

    slack pack

---
+++ images/stackstorm/bin/add-slack-pack.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+st2 pack install slack
+
+sed -i -e 's@webhook_url: "https://hooks.slack.com/services/<replace me>"@webhook_url: "'"$INCOMING_WEBHOOK_URL"'"@' \
+    -i -e 's@channel: "<DST_CHANNEL>"@channel: "'"$DST_CHANNEL"'"@' \
+    -i -e 's@username: "<SLACKBOT_NAME>"@username: "'"$SLACKBOT_NAME"'"@' \
+    -i -e 's@action_token: "<SLACKBOT_TOKEN>"@action_token: "'"$SLACKBOT_TOKEN"'"@' \
+    -i -e 's@- "<DST_CHANNEL>"@- "'"$DST_CHANNEL"'"@' \
+    /opt/stackstorm/configs/slack.yaml
+
+st2ctl reload --register-all 
