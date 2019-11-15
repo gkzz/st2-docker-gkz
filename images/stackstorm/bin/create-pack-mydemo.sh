d4bf0d6759af7ce81ab4dc0fe815d841c75cb982
gkzz <gakuji.tamaki@gmail.com>
2019-11-15 19:55:49 +0900

    mod mydemo-pack

--- images/stackstorm/bin/create-pack-mydemo.sh
+++ images/stackstorm/bin/create-pack-mydemo.sh
@@ -3,6 +3,6 @@
 PACKNAME="mydemo"
 
 cp -r /opt/stackstorm/packs.dev/$PACKNAME /opt/stackstorm/packs/
-st2 action create /opt/stackstorm/packs/$PACKNAME/actions/orquesta-examples-slack.yaml
-st2 action create /opt/stackstorm/packs/$PACKNAME/actions/orquesta-ping-with-items.yaml
+#st2 action create /opt/stackstorm/packs/$PACKNAME/actions/orquesta-examples-slack.yaml
+#st2 action create /opt/stackstorm/packs/$PACKNAME/actions/orquesta-ping-with-items.yaml
 st2 action list --pack=$PACKNAME | grep "orquesta-examples-slack"
