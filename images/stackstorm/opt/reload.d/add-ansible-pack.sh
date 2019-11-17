4cf78ab369e472cb14a7bc0727fec93ac8840d8e
gkzz <gakuji.tamaki@gmail.com>
2019-11-17 19:08:30 +0900

    mod reload

--- images/stackstorm/opt/reload.d/add-ansible-pack.sh
+++ images/stackstorm/opt/reload.d/add-ansible-pack.sh
@@ -9,9 +9,9 @@ if ! echo ${ANSIBLE_OUTPUT} > /dev/null 2>&1; then
     echo "[ Failed ] Add Ansible Pack"
 else
     echo "[ Succeeded ] Add Ansible Pack"
-    cp packs.dev/ansible/ansible.cfg /opt/stackstorm/packs/ansible/
+    cp -r /opt/stackstorm/packs.dev/ansible/* /opt/stackstorm/packs/ansible/*
     st2 run ansible.playbook \
-    inventory_file=/packs.dev/ansible/inventory/hosts \
-    playbook=/packs.dev/ansible/playbook/ping.yaml
+    inventory_file=/opt/stackstorm/packs/ansible/inventory/hosts \
+    playbook=/opt/stackstorm/packs/ansible/playbook/ping.yaml
 fi
 echo "ANSIBLE_PACK_INSTALLED: ${OUTPUT}" 
--- images/stackstorm/opt/reload.sh
+++ images/stackstorm/opt/reload.sh
@@ -2,7 +2,7 @@
 
 export ST2_API_KEY=$(st2 apikey create -k -m '{"used_by": "my integration"}')
 
-SCRIPTS = /opt/reload.d/*
+SCRIPTS=/opt/reload.d/*
 
 for f in $SCRIPTS; 
 do
