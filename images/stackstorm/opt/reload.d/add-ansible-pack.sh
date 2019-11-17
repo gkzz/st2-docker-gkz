26045612df93cc8bc28720f32012923beef60e44
gkzz <gakuji.tamaki@gmail.com>
2019-11-18 06:32:45 +0900

    mod ansible pack

--- images/stackstorm/opt/reload.d/add-ansible-pack.sh
+++ images/stackstorm/opt/reload.d/add-ansible-pack.sh
@@ -10,6 +10,15 @@ if ! echo ${ANSIBLE_OUTPUT} > /dev/null 2>&1; then
 else
     echo "[ Succeeded ] Add Ansible Pack"
     cp -r /opt/stackstorm/packs.dev/ansible/* /opt/stackstorm/packs/ansible/
+        sed -i -e 's|ansible_ssh_user: <ssh_user>|ansible_ssh_user: '"$SSH_USER"'|' \
+        -i -e 's|ansible_ssh_pass: <ssh_pass>|ansible_ssh_pass: '"$SSH_PASS"'|' \
+        /opt/stackstorm/packs/ansible/inventory/hosts
+
+    sed -i -e 's|webapp ansible_host=<webapp_ip>|webapp ansible_host='"$WEBAPP_IP"'|' \
+        -i -e 's|jumper ansible_port=<junos_port> ansible_host=<junos_ip>|
+                jumper ansible_port='"$JUNOS_PORT"' ansible_host='"$JUNOS_IP"'|' \
+        /opt/stackstorm/packs/ansible/inventory/group_vars/junos
+
     st2 run ansible.playbook \
     inventory_file=/opt/stackstorm/packs/ansible/inventory/hosts \
     playbook=/opt/stackstorm/packs/ansible/playbook/ping.yaml
