4effbcaff573b10eb677ed48e5ecf295ab11819b
gkzz <gakuji.tamaki@gmail.com>
2019-12-01 00:53:54 +0900

    mod mydemo

---
+++ packs.dev/mydemo/actions/scripts/build.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+function gitPull() 
+{
+    #cd $1
+    #OUTPUT=$(sudo git fetch | grep -E "$2\+->\s+origin/$2" | awk "{print $2}")
+    OUTPUT=$(sudo git fetch | grep -E "$1\+->\s+origin/$1" | awk "{print $2}")
+    for o in "$OUTPUT"
+    do
+        if [[ $o =~ origin/$1 ]]
+        then
+            sudo git checkout $1 \
+            && sudo git merge origin/$1
+            return 0
+        fi
+    done
+    return 1
+}
+
+
+function rmContainer()
+{
+  for c in $1;
+  do
+    for i in $(docker container ls | grep -E '$c.*Up' | awk '{print $1}');
+    do
+      docker container stop $i && docker container rm $i;
+      return 0
+    done
+  done
+  return 1
+}
+
+function main()
+{
+  OUTPUT=null
+  if [ gitPull $1 -eq 0 ]; then
+    if [ rmContainer $2 -eq 0 ]; then
+      OUTPUT=$(sudo docker-compose up -d --build)
+    fi
+    echo "GIT PULL: true"
+    echo "DOCKER BUILD: ${OUTPUT}"
+  fi
+  echo "GIT PULL: false"
+}
+
+#WORKING_DIR=$1
+#BRANCH=$2
+#CONTAINER_NAME=$3
+
+BRANCH=$1
+CONTAINER_NAME=$2
+
+#main WORKING_DIR BRANCH CONTAINER_NAME
+main BRANCH CONTAINER_NAME
