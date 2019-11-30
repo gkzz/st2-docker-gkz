36c975c3e735b9437dd1343b4cad1e03f44b16fb
gkzz <gakuji.tamaki@gmail.com>
2019-11-30 15:46:30 +0900

    mod mydemo

--- packs.dev/mydemo/actions/scripts/git_fetch.sh
+++ packs.dev/mydemo/actions/scripts/git_fetch.sh
@@ -3,7 +3,7 @@
 function main() 
 {
     #GIT_FETCH=$(git fetch | grep -E "master\+->\s+origin/master" | awk  "{print $1}")
-    OUTPUT=$(git fetch | grep -E "$1\+->\s+origin/$1" | awk "{print $2}")
+    OUTPUT=$(sudo git fetch | grep -E "$1\+->\s+origin/$1" | awk "{print $2}")
     for o in "$OUTPUT"
     do
         if [[ $o =~ origin/$1 ]]
