735ec475f0b0b0dc08d339a359b1faf39c2113d7
gkzz <gakuji.tamaki@gmail.com>
2019-11-30 14:33:56 +0900

    mod images/st2/opt/reload.d/clone_repo.sh

--- images/stackstorm/opt/reload.d/clone_repo.sh
+++ images/stackstorm/opt/reload.d/clone_repo.sh
@@ -2,5 +2,10 @@
 
 mkdir -p /usr/src/app \
     && chown -R root:st2packs app/ \
-    && cd /usr/src/app \
-    && git clone ${SECRET_GITHUB_CLONE_URL}
+    && cd /usr/src/app
+
+if [ ! -d ${SERVICE_DIR} ] 
+then
+    git clone \
+    "https://${GITHUB_SECRET_TOKEN}:x-oauth-basic@github.com/${GITHUB_ACCT}/${SERVICE_DIR}.git"
+fi
