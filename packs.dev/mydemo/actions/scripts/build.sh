d2f9d3604a406873451c5484e494239f0ac1febb
gkzz <gakuji.tamaki@gmail.com>
2019-12-01 18:59:48 +0900

    mod mydemo

--- packs.dev/mydemo/actions/scripts/build.sh
+++ packs.dev/mydemo/actions/scripts/build.sh
@@ -2,26 +2,35 @@
 
 function gitPull() 
 {
-    cd $1
-    OUTPUT=$(sudo git fetch | grep -E "$2\+->\s+origin/$2" | awk "{print $2}")
-    for o in "$OUTPUT"
-    do
-        if [[ $o =~ origin/$2 ]]
-        then
-            sudo git checkout $2 \
-            && sudo git merge origin/$2
-            return 0
-        fi
-    done
-    return 1
+  
+  WORKING_DIR=$1
+  REPO_URL=$2
+  BRANCH=$3
+  
+  if [ -d "$WORKING_DIR" ]; then
+    cd $WORKING_DIR
+    git fetch -p
+    git checkout -q $BRANCH
+    latest_rev=$(git ls-remote origin HEAD | awk '{print $1}')
+    current_rev=$(git rev-parse HEAD)
+    if [ "$latest_rev" != "$current_rev" ]; then
+      git reset --hard $(git log --pretty=format:%H | tail -1)
+      git pull
+      return 0
+    fi
+  else
+      git clone $REPO_URL $WORKING_DIR
+      return 0
+  fi
 }
 
 
 function rmContainer()
 {
-  for c in $1;
+  CONTAINER_NAME=$1
+  for c in $CONTAINER_NAME;
   do
-    for i in $(docker container ls | grep -E '$c.*Up' | awk '{print $1}');
+    for i in $(docker container ls | grep -E '\$c.*Up' | awk '{print $1}');
     do
       docker container stop $i && docker container rm $i;
       return 0
@@ -33,7 +42,7 @@ function rmContainer()
 function main()
 {
   OUTPUT=null
-  if [ $(gitPull $1 $2) -eq 0 ]; then
+  if [ $(gitPull $1 $2 $3 $4) -eq 0 ]; then
     if [ $(rmContainer $3) -eq 0 ]; then
       OUTPUT=$(sudo docker-compose up -d --build)
     fi
@@ -43,8 +52,10 @@ function main()
   echo "GIT PULL: false"
 }
 
-WORKING_DIR=$1
-BRANCH=$2
-CONTAINER_NAME=$3
 
-main WORKING_DIR BRANCH CONTAINER_NAME
+WORKING_DIR=$1
+REPO_URL=$2
+BRANCH=$3
+CONTAINER_NAME=$4
+
+main $WORKING_DIR $REPO_URL $BRANCH $CONTAINER_NAME
