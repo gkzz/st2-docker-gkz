23508ed49a25fb44bf95e970d41c5eb76126ada5
gkzz <gakuji.tamaki@gmail.com>
2019-12-07 22:08:39 +0900

    add poll_repo.yaml  ptn:

--- packs.dev/mydemo/actions/scripts/git_status.sh
+++ packs.dev/mydemo/actions/scripts/git_status.sh
@@ -6,16 +6,24 @@ function main()
 {
   working_dir=$1
   branch=$2
+  merged=$3
+
+  ptn=null
+  if "$merged"; then
+    ptn="Your branch is up to date with 'origin/$branch'." 
+  else
+    ptn="Your branch is behind 'origin/$branch'"
+  fi
+  
 
   counter=0
-  
   if [ -d "$working_dir" ]; then
     cd $working_dir
     sudo git fetch -p
     sudo git checkout -q $branch
-    latest_rev=$(sudo git ls-remote origin HEAD | awk '{print $1}')
-    current_rev=$(sudo git rev-parse HEAD)
-    if [ "$latest_rev" = "$current_rev" ]; then
+    status=$(git status | grep -E "$ptn")
+    status=$(echo ${status:-unknown})
+    if [ "$status" = "$ptn" ]; then
       counter=`expr $counter + 1`
     fi
   fi
@@ -27,11 +35,13 @@ function main()
 
 working_dir=$1
 branch=$2
+merged=$3
 
-counter=$(main $working_dir $branch)
+counter=$(main $working_dir $branch $merged)
 
-  if [ $counter -eq 1 ]; then
-    exit 0
-  else
-    exit 1
-  fi
+if [ $counter -eq 1 ]; then
+  exit 0
+fi
+
+exit 1
+
