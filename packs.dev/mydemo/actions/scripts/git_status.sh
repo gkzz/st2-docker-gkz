82ad1d2a95e021d2eb41e8107ce2579783cc86fe
gkzz <gakuji.tamaki@gmail.com>
2019-12-08 14:52:25 +0900

    hotfix

--- packs.dev/mydemo/actions/scripts/git_status.sh
+++ packs.dev/mydemo/actions/scripts/git_status.sh
@@ -15,13 +15,13 @@ function main()
     sudo git fetch -p
     sudo git checkout -q $branch
     if $up_to_date; then
-      output=$(sudo git status | grep -E "(Your)\s+(branch)\s+(is)\s+(up-to-date)\s+(with)\s+('origin/$branch')." | awk '{print $6') | grep -oP "$branch"
+      output=$(sudo git status | grep -E "(Your)\s+(branch)\s+(is)\s+(up-to-date)\s+(with)\s+('origin/$branch')." | awk '{print $6}' | grep -oP "$branch")
     else
-      output=$(sudo git status | grep -E "(Your)\s+(branch)\s+(is)\s+(behind)\s+('origin/$branch')\s+.*" | awk '{print $5}') | grep -oP "$branch"
+      output=$(sudo git status | grep -E "(Your)\s+(branch)\s+(is)\s+(behind)\s+('origin/$branch')\s+.*" | awk '{print $5}' | grep -oP "$branch")
     fi
 
     output=$(echo ${output:-unknown})
-    if [ "$output" = "'$branch'" ]; then
+    if [ "$output" = "$branch" ]; then
       counter=`expr $counter + 1`
     fi
   fi
