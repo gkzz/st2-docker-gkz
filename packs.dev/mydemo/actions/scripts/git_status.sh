18753064abdf0f4eda84361debb272ff3788be4f
gkzz <gakuji.tamaki@gmail.com>
2019-12-08 15:57:34 +0900

    mod poll_repo.yaml

--- packs.dev/mydemo/actions/scripts/git_status.sh
+++ packs.dev/mydemo/actions/scripts/git_status.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-set -e
-
 function main() 
 {
   working_dir=$1
   branch=$2
-  up_to_date=$3
+  expected=$3
 
   counter=0
 
@@ -14,13 +12,16 @@ function main()
     cd $working_dir
     sudo git fetch -p
     sudo git checkout -q $branch
-    if $up_to_date; then
+    if [ "$expected" -eq "up_to_date" ]; then
       output=$(sudo git status | grep -E "(Your)\s+(branch)\s+(is)\s+(up-to-date)\s+(with)\s+('origin/$branch')." | awk '{print $6}' | grep -oP "$branch")
-    else
+    elif [ "$expected" -eq "not_up_to_date" ]; then
       output=$(sudo git status | grep -E "(Your)\s+(branch)\s+(is)\s+(behind)\s+('origin/$branch')\s+.*" | awk '{print $5}' | grep -oP "$branch")
+    else
+      echo $counter
+      return
     fi
 
-    output=$(echo ${output:-unknown})
+    output=$(echo ${output:=unknown})
     if [ "$output" = "$branch" ]; then
       counter=`expr $counter + 1`
     fi
@@ -33,14 +34,15 @@ function main()
 
 working_dir=$1
 branch=$2
-up_to_date=$3
+expected=$3
 
-counter=$(main $working_dir $branch $up_to_date)
+counter=$(main $working_dir $branch $expected)
 
 
 if [ $counter -eq 1 ]; then
   exit 0
+else
+  exit 1
 fi
 
-exit 1
 
