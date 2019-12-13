64968bcf282e423a93d193ee50ef449cb66b2cc5
gkzz <gakuji.tamaki@gmail.com>
2019-12-13 23:30:44 +0900

    mod

--- images/stackstorm/opt/reload.d/create-pack-mydemo.sh
+++ images/stackstorm/opt/reload.d/create-pack-mydemo.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 pack="mydemo"
+repo="st2-mydemo-pack"
+
 
 if [ -d "/opt/stackstorm/packs" ]; then
   cd /opt/stackstorm/packs
   if [ ! -d "/opt/stackstorm/packs/$pack" ]; then
     git clone \
-      "https://${GITHUB_SECRET_TOKEN}:x-oauth-basic@github.com/${GITHUB_ACCT}/$pack.git"
+      "https://${GITHUB_SECRET_TOKEN}:x-oauth-basic@github.com/${GITHUB_ACCT}/$repo.git"
     chown -R root:st2packs $pack
   fi
 fi
