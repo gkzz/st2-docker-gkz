39f88d3aec0381548c5ee04f59f7f6345a42397a
gkzz <gakuji.tamaki@gmail.com>
2019-12-14 01:55:53 +0900

    hotfix

--- images/stackstorm/opt/reload.d/create-pack-mydemo.sh
+++ images/stackstorm/opt/reload.d/create-pack-mydemo.sh
@@ -6,8 +6,7 @@ pack_name="mydemo_pack"
 if [ -d "/opt/stackstorm/packs" ]; then
   cd /opt/stackstorm/packs
   if [ ! -d "/opt/stackstorm/packs/$pack_name" ]; then
-    st2 packs install \
-      "https://${GITHUB_SECRET_TOKEN}:x-oauth-basic@github.com/${GITHUB_ACCT}/$pack_name.git"
+    st2 packs install "https://github.com/${GITHUB_ACCT}/$pack_name.git"
     chown -R root:st2packs $pack_name
   fi
 fi
