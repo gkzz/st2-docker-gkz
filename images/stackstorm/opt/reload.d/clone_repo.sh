16ffcc24fda94f605a40da3185562e202ea70c81
gkzz <gakuji.tamaki@gmail.com>
2019-12-14 16:58:43 +0900

    add dc up -d to clone_repo.sh

--- images/stackstorm/opt/reload.d/clone_repo.sh
+++ images/stackstorm/opt/reload.d/clone_repo.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-mkdir -p /usr/src/app \
-    && cd $_
+mkdir -p /usr/src/app && cd $_
 
-if [ ! -d ${SERVICE_DIR} ] 
-then
+if [ ! -d ${SERVICE_DIR} ]; then
     git clone \
     "https://${GITHUB_SECRET_TOKEN}:x-oauth-basic@github.com/${GITHUB_ACCT}/${SERVICE_DIR}.git"
 fi
 
 chown -R root:st2packs ${SERVICE_DIR}
+
+if [ -d ${SERVICE_DIR} ]; then
+  cd ${SERVICE_DIR} && docker-compose up -d --build
+fi
