34375520b5734374b010aa0142f47cdc7c8bb6bd
gkzz <gakuji.tamaki@gmail.com>
2019-12-16 21:18:18 +0900

    write install -r requirements.txt.dev

--- images/stackstorm/opt/reload.d/create-pack-mydemo.sh
+++ images/stackstorm/opt/reload.d/create-pack-mydemo.sh
@@ -7,6 +7,8 @@ if [ -d "/opt/stackstorm/packs" ]; then
   cd /opt/stackstorm/packs
   if [ ! -d "/opt/stackstorm/packs/$pack_name" ]; then
     st2 pack install "https://github.com/${GITHUB_ACCT}/$pack_name.git"
+    sudo /opt/stackstorm/virtualenvs/ansible/bin/pip install --upgrade pip
+    sudo /opt/stackstorm/virtualenvs/$pack_name/bin/pip install -r requirements.txt.dev
     chown -R root:st2packs $pack_name
   fi
 fi
