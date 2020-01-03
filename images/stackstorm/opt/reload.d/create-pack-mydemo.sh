4cd2740d9230af6248e1239d88aef3fb16b98ac0
gkzz <gakuji.tamaki@gmail.com>
2020-01-04 00:10:27 +0900

    mod register: result

--- images/stackstorm/opt/reload.d/create-pack-mydemo.sh
+++ images/stackstorm/opt/reload.d/create-pack-mydemo.sh
@@ -7,8 +7,8 @@ if [ -d "/opt/stackstorm/packs" ]; then
   cd /opt/stackstorm/packs
   if [ ! -d "/opt/stackstorm/packs/$pack_name" ]; then
     st2 pack install "https://github.com/${GITHUB_ACCT}/$pack_name.git"
-    /opt/stackstorm/virtualenvs/ansible/bin/pip install --upgrade pip
-    /opt/stackstorm/virtualenvs/$pack_name/bin/pip install -r /st2-docker/opt/requirements.txt.dev
+    #/opt/stackstorm/virtualenvs/ansible/bin/pip install --upgrade pip
+    #/opt/stackstorm/virtualenvs/$pack_name/bin/pip install -r /st2-docker/opt/requirements.txt.dev
     chmod -R 755 /opt/stackstorm/packs/$pack_name
     chown -R root:st2packs /opt/stackstorm/packs/$pack_name
   fi
